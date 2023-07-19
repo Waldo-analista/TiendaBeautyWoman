a974364f9a06dd2a4e32b8efb194b89dd7001f98
Waldo Hidalgo <whidalgohp@gmail.com>
2023-07-19 15:27:38 -0400

    Se solucionan rutas

--- js/verProductos.js
+++ js/verProductos.js
@@ -1,12 +1,12 @@
 const boton_verAllProductos=document.querySelector('.banner__boton');
-boton_verAllProductos.addEventListener("click",()=>window.location.href='../productosShow.html')
+boton_verAllProductos.addEventListener("click",()=>window.location.assign('/../productosShow.html'))
 
 
 const boton_verGymProducts=document.querySelector('.gym__boton_catalogo');
-boton_verGymProducts.addEventListener("click",()=>window.location.assign('../productosShow.html?categoria=gym'));
+boton_verGymProducts.addEventListener("click",()=>window.location.assign('/../productosShow.html?categoria=gym'));
 
 const boton_verOficinaProducts=document.querySelector('.oficina__boton_catalogo');
-boton_verOficinaProducts.addEventListener("click",()=>window.location.assign('../productosShow.html?categoria=oficina'));
+boton_verOficinaProducts.addEventListener("click",()=>window.location.assign('/../productosShow.html?categoria=oficina'));
 
 const boton_verCasualProducts=document.querySelector('.casual__boton_catalogo');
-boton_verCasualProducts.addEventListener("click",()=>window.location.assign('../productosShow.html?categoria=casual'));
+boton_verCasualProducts.addEventListener("click",()=>window.location.assign('/../productosShow.html?categoria=casual'));
